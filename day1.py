336cdccf08a39c164e00774d40ed43e5a6cdeebb
Sara <sara.ferro.5@gmail.com>
2021-01-02 11:52:09 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -5,21 +5,23 @@ def elem_wise_1DAddition(x,y):
     assert (len(x.shape)==1 and len(x.shape) == len(y.shape))
     out = np.zeros(x.shape[0])
     print("shape of x: ", x.shape)
-    print("length of x: ", len(x.shape))
+    print(len(x.shape),"-D vector x")
     print("shape of y: ", y.shape)
-    print("length of y: ", len(y.shape))
-    for i in range(0,x.shape[0]):
+    print(len(y.shape),"-D vector y")
+    # range begins by default from 0 if we set one only index
+    for i in range(x.shape[0]):
         out[i] = x[i]+y[i]
     print("output vector: ", out)
     return out
 
 # broadcasting operations
 def broadcasting_addition(x,y):
-    # assert if the following conditions are not verified
+    # there is not necessity to assert for the different dimension
+    # of the arrays for the broadcasting of the operations
     print("shape of x: ", x.shape)
-    print("length of x: ", len(x.shape))
+    print(len(x.shape),"-D vector x")
     print("shape of y: ", y.shape)
-    print("length of y: ", len(y.shape))
+    print(len(y.shape),"-D vector y")
     out = x+y
     print("output vector: ", out)
     return out
@@ -27,13 +29,14 @@ def broadcasting_addition(x,y):
 # test
 x1 = np.array([1,1,1])
 y1 = np.array([2,2,2])
-z1 = np.array([2])
+# the one element array can be specified without square brackets
+z1 = np.array(2)
 elem_wise_1DAddition(x1,y1)
-# the one one element of z is broadcasted to have the same
+# the one element of z is broadcasted to have the same
 # dimension of the other summed array (vector-1D tensor)
 broadcasting_addition(x1,z1)
 x2 = np.array([[1,1,1],
               [1,1,1]])
-# the one one element of z is broadcasted to have the same
+# the one element of z is broadcasted to have the same
 # dimension of the other summed array (matrix-2D tensor)
 broadcasting_addition(x2,z1)
